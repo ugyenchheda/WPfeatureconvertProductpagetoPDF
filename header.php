05e447852d080261a562ba708ffa2cf2b36bae16
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-04-07 22:56:42 +0300

    Update header.php

--- header.php
+++ header.php
@@ -3,12 +3,8 @@
  * The Header template for our theme
  */
 ?><!DOCTYPE html>
-<!--[if IE 8]>
 <html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
